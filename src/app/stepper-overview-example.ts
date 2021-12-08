e1f60002da655ec2aff933c4dbb829ab9e6ae676
Ravi Chandhar P.B <calibraint@Ravis-MacBook-Pro-2.local>
2021-12-08 16:55:31 +0530

    Added additional fields

--- src/app/stepper-overview-example.ts
+++ src/app/stepper-overview-example.ts
@@ -21,23 +21,34 @@ export class StepperOverviewExample implements OnInit {
 
   ngOnInit() {
     this.personDetailsFormGroup = this._formBuilder.group({
-      nameCtrl: ['', Validators.required],
+      lastNameCtrl: ['', Validators.required],
+      dobCtrl: ['', Validators.required],
+      firstNameCtrl:  ['', Validators.required],
+      dlCtrl: ['', Validators.required],
       emailCtrl: ['', Validators.required],
+      phoneCtrl: ['', Validators.required],
       ssnCtrl: ['', Validators.required],
-      countryCtrl: ['', Validators.required],
+      zipCtrl: ['', Validators.required],
+      udfCtrl: ['', Validators.required]
     });
     this.geoLocationFormGroup = this._formBuilder.group({
       address1Ctrl: ['', Validators.required],
       address2Ctrl: ['', Validators.required],
       zipCtrl: ['', Validators.required],
+      cityCtrl: ['', Validators.required],
+      stateCtrl: ['', Validators.required],
     });
     this.previousMedicalFormGroup = this._formBuilder.group({
       descriptionCtrl: ['', Validators.required],
       bloodGroupCtrl: ['', Validators.required],
+      sbCtrl: ['', Validators.required],
+      dbpCtrl: ['', Validators.required],
+      prCtrl: ['', Validators.required],
     });
     this.insuranceFormGroup = this._formBuilder.group({
       companyCtrl: ['', Validators.required],
       policyCtrl: ['', Validators.required],
+      expiryCtrl: ['', Validators.required],
     });
     this.paymentFormGroup = this._formBuilder.group({
       paymentCtrl: ['', Validators.required],
